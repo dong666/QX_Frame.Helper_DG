37682771f06a169ca6fa74de9a077fc7b5678422
qixiao <wd8622088@foxmail.com>
2017-07-27 11:31:17 +0800

    add Log_MQ_Service

---
+++ 10-Code/QX_Frame.Helper_DG_NETFramework461/Log/Log.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * author:qixiao
+ * create:2017-7-24 17:04:49
+ * */
+namespace QX_Frame.Helper_DG.Log
+{
+    /// <summary>
+    /// enum LogType
+    /// </summary>
+    public enum LogType
+    {
+        /// <summary>
+        /// general log
+        /// </summary>
+        log,
+        /// <summary>
+        /// use log
+        /// </summary>
+        use,
+        /// <summary>
+        /// error log
+        /// </summary>
+        error
+    }
+
+    /// <summary>
+    /// log class
+    /// </summary>
+    public class Log
+    {
+        public string type { get; set; } = nameof(LogType.log);
+        public string date { get => DateTime.Now.ToLongDateString(); }
+        public string time { get => DateTime.Now.ToLongTimeString(); }
+        public string client_ip { get; set; }
+        public string server_name { get => Ip_Helper_DG.GetServerHostName(); }
+        public string server_ip { get => Ip_Helper_DG.GetServerIpAddress(); }
+        public string uri { get; set; }
+        public string parameters { get; set; }
+        public string message { get; set; }
+
+        public Log(LogType logType) => this.type = nameof(logType);
+
+        public Log(LogType logType, string message)
+        {
+            this.type = nameof(logType);
+            this.message = message;
+        }
+
+        public Log(LogType logType, HttpRequestMessage request)
+        {
+            this.type = nameof(logType);
+            this.client_ip = request.GetIpAddressFromRequest();
+            this.uri = request.RequestUri.ToString();
+        }
+        public Log(LogType logType, HttpRequestMessage request, string message)
+        {
+            this.type = nameof(logType);
+            this.client_ip = request.GetIpAddressFromRequest();
+            this.uri = request.RequestUri.ToString();
+            this.message = message;
+        }
+
+        /// <summary>
+        /// Convert object to json string
+        /// </summary>
+        /// <returns></returns>
+        public string ToJson() => Convert_Helper_DG.T_To_Json(this).ToString();
+    }
+}
